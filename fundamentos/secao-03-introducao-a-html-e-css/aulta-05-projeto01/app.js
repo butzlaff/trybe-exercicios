bd8ae83ca68bd073241125ae4892fb54aee61b10
Emilio Butzlaff <emiliobutz@gmail.com>
2022-11-14 16:13:16 -0300

    Adicionando update ao app.js

--- fundamentos/secao-03-introducao-a-html-e-css/aulta-05-projeto01/app.js
+++ fundamentos/secao-03-introducao-a-html-e-css/aulta-05-projeto01/app.js
@@ -1,3 +1,9 @@
 function changeColor(divId){
-    document.getElementById(divId.id).style.backgroundColor = 'red'
+    document.getElementById(divId.id).style.backgroundColor = localStorage.setItem()
 }
+
+function getColor(color){
+    localStorage.getItem(color)
+    console.log(color)
+}
+
