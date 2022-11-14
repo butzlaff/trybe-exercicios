5a78938d9d819898f8949056b601093d182a6f7b
Emilio Butzlaff <emiliobutz@gmail.com>
2022-11-14 16:40:23 -0300

    Adicionando função que troca cores

--- fundamentos/secao-03-introducao-a-html-e-css/aulta-05-projeto01/app.js
+++ fundamentos/secao-03-introducao-a-html-e-css/aulta-05-projeto01/app.js
@@ -1,9 +1,10 @@
 function changeColor(divId){
-    document.getElementById(divId.id).style.backgroundColor = localStorage.setItem()
+    console.log(localStorage.getItem('cor'))
+    document.getElementById(divId.id).style.backgroundColor = localStorage.getItem('cor')
 }
 
 function getColor(color){
-    localStorage.getItem(color)
-    console.log(color)
+//    localStorage.removeItem(color.id)
+    localStorage.setItem('cor', `${color.id}`)
 }
 
