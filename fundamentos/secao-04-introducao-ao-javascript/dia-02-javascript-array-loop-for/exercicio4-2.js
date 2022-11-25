cb945653a558c7b98b5bcf4352d6efc47922db54
Emilio Butzlaff <emiliobutz@gmail.com>
2022-11-25 14:57:32 -0300

    Utilizando for, descubra qual o maior valor contido no array e imprima-o;

--- fundamentos/secao-04-introducao-ao-javascript/dia-02-javascript-array-loop-for/exercicio4-2.js
+++ fundamentos/secao-04-introducao-ao-javascript/dia-02-javascript-array-loop-for/exercicio4-2.js
@@ -24,14 +24,25 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // # Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, 
 // # imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
 
-let resultado = 0;
-for (let number of numbers){
-    resultado += number;
-}
-let resultadoFinal = resultado / numbers.length;
+// let resultado = 0;
+// for (let number of numbers){
+//     resultado += number;
+// }
+// let resultadoFinal = resultado / numbers.length;
 
-if (resultadoFinal > 20){ 
-    console.log('Valor maior que 20');
-}else{
-    console.log('Valor menos que 20');
-}
+// if (resultadoFinal > 20){ 
+//     console.log('Valor maior que 20');
+// }else{
+//     console.log('Valor menos que 20');
+// }
+
+// # Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+
+// let greatestNumber = 0;
+
+// for (let i = 0; i < numbers.length; i++){
+//     if(numbers[i] > greatestNumber)
+//         greatestNumber = numbers[i];
+// }
+
+// console.log(greatestNumber);
