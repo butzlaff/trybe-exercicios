3e6f7f140396050aea54c626bcf36351f2639e07
Emilio Butzlaff <emiliobutz@gmail.com>
2022-11-25 15:02:34 -0300

    Descubra quantos valores ímpares existem no array e imprima o resultado

--- fundamentos/secao-04-introducao-ao-javascript/dia-02-javascript-array-loop-for/exercicio4-2.js
+++ fundamentos/secao-04-introducao-ao-javascript/dia-02-javascript-array-loop-for/exercicio4-2.js
@@ -46,3 +46,17 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // }
 
 // console.log(greatestNumber);
+
+// # Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “nenhum valor ímpar encontrado”;
+
+let oddNumbers = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 > 0)
+        oddNumbers++;
+}
+if (oddNumbers > 0) {
+    console.log(`Foram encontrados ${oddNumbers} números ímpares`);
+} else {
+    console.log('Nenhum valor ímpar encontrado')
+}
