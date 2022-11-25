a54e1a1b27b7f48ab6dde44cd8c5bab44f7d9868
Emilio Butzlaff <emiliobutz@gmail.com>
2022-11-25 14:49:19 -0300

    caso o valor final seja maior que 20

--- fundamentos/secao-04-introducao-ao-javascript/dia-02-javascript-array-loop-for/exercicio4-2.js
+++ fundamentos/secao-04-introducao-ao-javascript/dia-02-javascript-array-loop-for/exercicio4-2.js
@@ -2,21 +2,36 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // # Percorra o array imprimindo todos os valores nele contidos com a função console.log();
 // for(let number of numbers){
-//     console.log(number)
+//     console.log(number);
 // }
 
 // # Some todos os valores contidos no array e imprima o resultado;
 
 // let resultado = 0
 // for (let number of numbers){
-//     resultado += number 
+//     resultado += number;
 // }
-// console.log(resultado)
+// console.log(resultado);
 
 // # Calcule e imprima a média aritmética dos valores contidos no array;
 
-// let resultado = 0
+// let resultado = 0;
 // for (let number of numbers){
-//     resultado += number 
+//     resultado += number;
 // }
 // console.log(resultado / numbers.length)
+
+// # Com base no código que acabou de gerar, faça com que, caso o valor final seja maior que 20, 
+// # imprima a mensagem: “valor maior que 20”. Caso não seja, imprima a mensagem: “valor menor ou igual a 20”;
+
+let resultado = 0;
+for (let number of numbers){
+    resultado += number;
+}
+let resultadoFinal = resultado / numbers.length;
+
+if (resultadoFinal > 20){ 
+    console.log('Valor maior que 20');
+}else{
+    console.log('Valor menos que 20');
+}
