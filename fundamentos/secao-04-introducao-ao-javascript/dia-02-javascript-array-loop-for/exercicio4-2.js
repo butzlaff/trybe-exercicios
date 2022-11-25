efa7f1ded2d06970a641b6fc6092467c7fbbbadb
Emilio Butzlaff <emiliobutz@gmail.com>
2022-11-25 14:43:49 -0300

    Some todos os valores contidos no array e imprima o resultado;

--- fundamentos/secao-04-introducao-ao-javascript/dia-02-javascript-array-loop-for/exercicio4-2.js
+++ fundamentos/secao-04-introducao-ao-javascript/dia-02-javascript-array-loop-for/exercicio4-2.js
@@ -1,5 +1,14 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for(let number of numbers){
-    console.log(number)
-}
+// # Percorra o array imprimindo todos os valores nele contidos com a função console.log();
+// for(let number of numbers){
+//     console.log(number)
+// }
+
+// # Some todos os valores contidos no array e imprima o resultado;
+
+// let resultado = 0
+// for (let number of numbers){
+//     resultado += number 
+// }
+// console.log(resultado)
