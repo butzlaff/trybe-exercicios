021c8e591d1b06a77928dddb70370206dcf42df3
Emilio Butzlaff <emiliobutz@gmail.com>
2022-11-30 16:32:26 -0300

    exercicio 05

---
+++ fundamentos/secao-04-introducao-ao-javascript/dia-04-javascript-objetos-e-funcoes/exercicio06.js
@@ -0,0 +1,24 @@
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+  leitor.livrosFavoritos.push({
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+  })
+
+  console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama ${leitor['livrosFavoritos'][0]['titulo']}.`)
+
+  console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama ${leitor.livrosFavoritos[0].titulo}.`)
+
+  console.table(leitor.livrosFavoritos[1])
