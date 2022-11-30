2145d98a7dd0abdfbfda1694eb12bf0f9bb7a78c
Emilio Butzlaff <emiliobutz@gmail.com>
2022-11-30 16:43:43 -0300

    exercicio 06

--- fundamentos/secao-04-introducao-ao-javascript/dia-04-javascript-objetos-e-funcoes/exercicio06.js
+++ fundamentos/secao-04-introducao-ao-javascript/dia-04-javascript-objetos-e-funcoes/exercicio06.js
@@ -20,5 +20,3 @@ let leitor = {
   console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama ${leitor['livrosFavoritos'][0]['titulo']}.`)
 
   console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama ${leitor.livrosFavoritos[0].titulo}.`)
-
-  console.table(leitor.livrosFavoritos[1])
