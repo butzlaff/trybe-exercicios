c9806c9b1afcab064b14e826496733cf4d09a9c2
Emilio Butzlaff <emiliobutz@gmail.com>
2022-11-30 14:50:45 -0300

    Exercicios de Fundamentos modulo 4 dia 02

--- fundamentos/secao-04-introducao-ao-javascript/dia-02-javascript-array-loop-for/arrayloopfor.js
+++ fundamentos/secao-04-introducao-ao-javascript/dia-02-javascript-array-loop-for/arrayloopfor.js
@@ -1,5 +1,22 @@
-let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+// let names = ['João', 'Maria', 'Antônio', 'Margarida'];
 
-for (let name of names){
-    console.log(name)
-}
+let names = {
+    name: 'João', 
+    age: 30,
+    city: 'São Paulo',
+    country: 'Brasil'
+};
+
+// for (let name of names){
+//     console.log(name)
+// }
+
+// for (let name in names){
+//     console.log(names[0])
+// }
+
+// for (let name = 0; name < names.length; name += 1){
+//     console.log(names[name])
+// }
+
+console.log(names['age'])
--- fundamentos/secao-04-introducao-ao-javascript/dia-02-javascript-array-loop-for/duvidas.js
+++ fundamentos/secao-04-introducao-ao-javascript/dia-02-javascript-array-loop-for/duvidas.js
@@ -1,12 +1,27 @@
-let arrayNumber = [2, 4, 5, 1, 3]
+// let arrayNumber = [2, 4, 5, 1, 3];
+// let arrayNumber = ['João', 'Maria', 'Antônio', 'Margarida'];
+// let number = new Number;
 
+// for (let i = 0; i < arrayNumber.length; i++) {
+//     // console.log(i)
+//     for (let index = 0; index < arrayNumber.length; index++){
+//     if (arrayNumber[i] < arrayNumber[index]){
+//         [arrayNumber[i], arrayNumber[index]] = [arrayNumber[index], arrayNumber[i]];
+//     }
+// }
+// }
+// console.log(arrayNumber[0])
 
-for (let i = 0; i < arrayNumber.length; i++) {
-    // console.log(i)
-    for (let index = 0; index < arrayNumber.length; index++){
-    if (arrayNumber[i] < arrayNumber[index]){
-        [arrayNumber[i], arrayNumber[index]] = [arrayNumber[index], arrayNumber[i]];
-    }
-}
-}
-console.log(arrayNumber)
+// if (arrayNumber.indexOf(number) >= 0) {
+//     console.log(arrayNumber.indexOf(number));
+// } else 
+//     console.log("Elemento não foi encontrado.");
+
+// let namesJson = arrayNumber.map(item => JSON.parse(item))
+// console.log(namesJson)
+
+function sum(a, b) {
+    a + b;
+  }
+  console.log(sum(1, 2));
+  
