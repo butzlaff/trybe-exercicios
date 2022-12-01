faccb353bb6bb8ff467e355ba756cbaf49ec011a
Emilio Butzlaff <emiliobutz@gmail.com>
2022-12-01 15:45:19 -0300

    exercicio 06

--- fundamentos/secao-04-introducao-ao-javascript/dia-04-javascript-objetos-e-funcoes/exercicio05.js
+++ fundamentos/secao-04-introducao-ao-javascript/dia-04-javascript-objetos-e-funcoes/exercicio05.js
@@ -1,23 +1,32 @@
-function Personagens(personagem, origem, nota, recorrente){
-    this.personagem = personagem;
-    this.origem = origem;
-    this.nota = nota;
-    this.recorrente = recorrente;
-  }
-  
-  let margarida = new Personagens('Margarida', 'Pato Donald', 'Namorada do personagem principal nos quadrinhos do Pato Donald', 'Sim')
-  
-  let tioPatinhas = new Personagens('Tio Patinhas', 'Christmas on Bear Mountain, Dell’s Four Color Comics #178', 'O último MacPatinhas', 'Sim')
-  
-  for (let key in margarida){
-    if(margarida[key] === tioPatinhas[key]){
-        console.log('Ambos decorrentes')
-    }else{
-    console.log(margarida[key] + ' e ' + tioPatinhas[key])
-}  
+function Personagens(personagem, origem, nota, recorrente) {
+  this.personagem = personagem;
+  this.origem = origem;
+  this.nota = nota;
+  this.recorrente = recorrente;
 }
 
+let margarida = new Personagens(
+  "Margarida",
+  "Pato Donald",
+  "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  "Sim"
+);
 
-  console.table(margarida)
+let tioPatinhas = new Personagens(
+  "Tio Patinhas",
+  "Christmas on Bear Mountain, Dell’s Four Color Comics #178",
+  "O último MacPatinhas",
+  "Sim"
+);
 
-  console.table(tioPatinhas)
+for (let key in margarida) {
+  if (margarida[key] === tioPatinhas[key]) {
+    console.log("Ambos decorrentes");
+  } else {
+    console.log(margarida[key] + " e " + tioPatinhas[key]);
+  }
+}
+
+console.table(margarida);
+
+console.table(tioPatinhas);
--- fundamentos/secao-04-introducao-ao-javascript/dia-04-javascript-objetos-e-funcoes/exercicio06.js
+++ fundamentos/secao-04-introducao-ao-javascript/dia-04-javascript-objetos-e-funcoes/exercicio06.js
@@ -1,22 +1,26 @@
 let leitor = {
-    nome: 'Julia',
-    sobrenome: 'Pessoa',
-    idade: 21,
-    livrosFavoritos: [
-      {
-        titulo: 'O Pior Dia de Todos',
-        autor: 'Daniela Kopsch',
-        editora: 'Tordesilhas',
-      },
-    ],
-  };
+  nome: "Julia",
+  sobrenome: "Pessoa",
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: "O Pior Dia de Todos",
+      autor: "Daniela Kopsch",
+      editora: "Tordesilhas",
+    },
+  ],
+};
 
-  leitor.livrosFavoritos.push({
-    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
-    autor: 'JK Rowling',
-    editora: 'Rocco',
-  })
+leitor.livrosFavoritos.push({
+  titulo: "Harry Potter e o Prisioneiro de Azkaban",
+  autor: "JK Rowling",
+  editora: "Rocco",
+});
 
-  console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama ${leitor['livrosFavoritos'][0]['titulo']}.`)
+console.log(
+  `O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama ${leitor["livrosFavoritos"][0]["titulo"]}.`
+);
 
-  console.log(`O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama ${leitor.livrosFavoritos[0].titulo}.`)
+console.log(
+  `O livro favorito de ${leitor.nome} ${leitor.sobrenome} se chama ${leitor.livrosFavoritos[0].titulo}.`
+);
