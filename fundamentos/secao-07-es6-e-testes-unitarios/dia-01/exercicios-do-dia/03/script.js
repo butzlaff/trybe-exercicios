11db986efe366f6f65e33100bd1902626563dd45
Emilio Butzlaff <emiliobutz@gmail.com>
2023-01-19 11:03:13 -0300

    Finalizando exercicios

--- fundamentos/secao-07-es6-e-testes-unitarios/dia-01/exercicios-do-dia/03/script.js
+++ fundamentos/secao-07-es6-e-testes-unitarios/dia-01/exercicios-do-dia/03/script.js
@@ -54,3 +54,56 @@ const allLessons = Object.assign({}, {lesson1, lesson2, lesson3})
 
 console.log(allLessons)
 
+// Usando o objeto criado no tópico anterior, crie uma função que retorne o número total de estudantes em todas as aulas.
+// Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
+
+const sumStudents = (obj) => {
+  let sum = 0;
+  for (let index = 0; index < Object.values(obj).length; index += 1) {
+    sum += Object.values(obj)[index].numeroEstudantes;
+
+  }
+  return sum;
+}
+
+console.log(sumStudents(allLessons))
+
+
+// Crie uma função que verifique se o par (chave / valor) existe na função. 
+// Essa função deve possuir três parâmetros, sendo eles o objeto, o nome da chave e o valor da chave. Exemplo:
+
+// Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
+
+const getValueByNumber = (obj, key) => {
+  return (Object.values(obj)[key])
+}
+
+console.log(getValueByNumber(lesson1, 0));
+// Output: 'Matemática'
+
+
+
+const verifyPair = (obj, key, value) => {
+  return (obj[key] === value)
+}
+
+console.log(verifyPair(lesson3, 'turno', 'noite'));
+// Output: true,
+console.log(verifyPair(lesson3, 'materia', 'Maria Clara'));
+// Output: false
+
+// Utilizando o objeto (allLesson), crie uma função para contar quantos estudantes assistiram às aulas de Matemática;
+
+// Utilizando o objeto (allLesson), crie uma função que deva retornar um objeto que represente 
+// o relatório do professor ou professora, as aulas que ele ou ela ministrou e o número total de estudantes.
+
+// const getNumberOfStudentsMath = (obj) => {
+//   const key = Object.keys(obj);
+//   const sumStudent = 0
+//   for (let index = 0; index < key.length; index += 1) {
+//     if ()
+//   }
+
+// }
+
+// console.log(getNumberOfStudentsMath(allLessons));
